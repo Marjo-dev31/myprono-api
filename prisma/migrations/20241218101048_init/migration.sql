882da37d0a3da2a515df02afe8e79acb3d409f82
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-12-18 11:14:20 +0100

    feat(prisma): create migration file

---
+++ prisma/migrations/20241218101048_init/migration.sql
@@ -0,0 +1,94 @@
+-- CreateTable
+CREATE TABLE `Competition` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Competition_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Contest` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(3) NOT NULL,
+    `team1Id` INTEGER NOT NULL,
+    `team2Id` INTEGER NOT NULL,
+    `winnerTeam` INTEGER NOT NULL,
+    `offensiveBonus` BOOLEAN NOT NULL DEFAULT false,
+    `defensiveBonus` BOOLEAN NOT NULL DEFAULT false,
+    `competitionId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Team` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `teamName` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Team_teamName_key`(`teamName`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstname` VARCHAR(191) NOT NULL,
+    `lastname` VARCHAR(191) NOT NULL,
+    `mail` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `pseudo` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_mail_key`(`mail`),
+    UNIQUE INDEX `User_pseudo_key`(`pseudo`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Bet` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `matchId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `betTeamId` INTEGER NOT NULL,
+    `offensiveBonus` BOOLEAN NOT NULL DEFAULT false,
+    `defensiveBonus` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Score` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `total` INTEGER NOT NULL DEFAULT 0,
+    `competitionId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Contest` ADD CONSTRAINT `Contest_team1Id_fkey` FOREIGN KEY (`team1Id`) REFERENCES `Team`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Contest` ADD CONSTRAINT `Contest_team2Id_fkey` FOREIGN KEY (`team2Id`) REFERENCES `Team`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Contest` ADD CONSTRAINT `Contest_winnerTeam_fkey` FOREIGN KEY (`winnerTeam`) REFERENCES `Team`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Contest` ADD CONSTRAINT `Contest_competitionId_fkey` FOREIGN KEY (`competitionId`) REFERENCES `Competition`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Bet` ADD CONSTRAINT `Bet_matchId_fkey` FOREIGN KEY (`matchId`) REFERENCES `Contest`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Bet` ADD CONSTRAINT `Bet_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Bet` ADD CONSTRAINT `Bet_betTeamId_fkey` FOREIGN KEY (`betTeamId`) REFERENCES `Team`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Score` ADD CONSTRAINT `Score_competitionId_fkey` FOREIGN KEY (`competitionId`) REFERENCES `Competition`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Score` ADD CONSTRAINT `Score_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
